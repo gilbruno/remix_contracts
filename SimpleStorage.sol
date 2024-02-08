8bed3f03c109959676c79864732d4f998f5ead47
Gilles Bruno <gilles_bruno@gmx.fr>
2024-02-08 11:22:41 +0100

    add other SC

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -17,7 +17,7 @@ contract SimpleStorage {
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public virtual {
+    function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
 
